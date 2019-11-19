bb156a0ac785ccae6d79260f467dbc424627e9a1
Markus <m.walther97@gmail.com>
2019-11-19 23:29:36 +0100

    ci: run linting + coverage (#1)

---
+++ bin/coverage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+go test -coverprofile=coverage_release.part -v ./...
+
+go test -coverprofile=coverage_panic.part -v -tags unassert_panic ./...
+
+go test -coverprofile=coverage_stderr.part -v -tags unassert_stderr ./...
+
+echo "mode: set" >coverage.txt
+grep -h -v "mode: set" *.part >>coverage.txt
