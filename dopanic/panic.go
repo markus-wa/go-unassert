f13866403861b49551b36483c72a3c37ddab60b8
Markus Walther <m.walther97@gmail.com>
2019-11-20 01:24:36 +0100

    godoc for stderr & dopanic

--- dopanic/panic.go
+++ dopanic/panic.go
@@ -1,9 +1,11 @@
+// Package dopanic implements unassert.ErrorHandler by panicking.
 package dopanic
 
 import (
 	"fmt"
 )
 
+// Panic formats an error and panics.
 func Panic(format string, v ...interface{}) {
 	panic(fmt.Sprintf(format, v...))
 }
--- stderr/stderr.go
+++ stderr/stderr.go
@@ -1,3 +1,4 @@
+// Package stderr implements unassert.ErrorHandler by printing to stderr.
 package stderr
 
 import (
@@ -8,6 +9,7 @@ import (
 
 var stderr io.Writer = os.Stderr
 
+// Stderr prints an error to stderr.
 func Stderr(format string, v ...interface{}) {
 	_, err := fmt.Fprintf(stderr, format, v...)
 	fmt.Println("failed to write to stderr:", err)
