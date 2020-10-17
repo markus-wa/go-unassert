e26533a026aa5ed703f039eb6843ef302dff20b5
Markus Walther <m.walther97@gmail.com>
2020-10-17 14:00:28 +0100

    readme: add lazy eval functions example

--- example/example.go
+++ example/example.go
@@ -18,4 +18,15 @@ func main() {
 	unassert.Error("you can also %s a string", "format")
 	unassert.Nilf(new(struct{}), "you can also pass error messages to any other function")
 	unassert.NotNilf(nil, "with or without %s", "formatting")
+
+	lenLessThan10 := func(v interface{}) bool {
+		return len(v.([]int)) < 10
+	}
+	unassert.Matches(lenLessThan10, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+
+	complexEvaluation := func() bool {
+		// add logic here
+		return false
+	}
+	unassert.ReturnsTrue(complexEvaluation)
 }
